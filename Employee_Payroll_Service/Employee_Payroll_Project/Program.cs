cef40e4b9c2dfbac369466bc2e824ccd2ea46d23
Mohanee <mohanee99@gmail.com>
2020-11-04 22:15:25 +0530

    [mohanee]Refactor Files

---
+++ Employee_Payroll_Service/Employee_Payroll_Project/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Employee_Payroll_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            EmployeeModel model = new EmployeeModel();
+            EmployeeRepo obj = new EmployeeRepo();
+            obj.AddEmployee(model);
+        }
+    }
+}
